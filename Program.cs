f8691416aaa8e3f7ff83d55ea2e0f11b8e434ebb
Thijn <thijnkroon@gmail.com>
2021-11-30 15:41:10 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ResearchDashboard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            BibliograhpyAPI api = new DblpAPI();
+            // As an example, this will fetch the XML of a specific author
+            api.FetchXML("https://dblp.org/pid/65/9612.xml");
+        }
+    }
+}
